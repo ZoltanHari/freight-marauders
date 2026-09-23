ce0d523fdd9947bc4f7fc32cd75eb1d86ee6e47c
ati hun <harizozi1009@gmail.com>
2026-09-23 10:04:06 -0600

    Enter and Exit Boat

--- Scripts/Boat.cs
+++ Scripts/Boat.cs
@@ -5,15 +5,25 @@ public partial class Boat : CharacterBody2D
 {
 	[Export] public float Speed { get; set; } = 150f;
     [Export] public float RotationSpeed { get; set; } = 3.0f; 
-	public bool Boat_Can_Move = false;
+	public bool Player_In_Boat = false;
+	public bool isPlayerNear = false;
+
+	public Player player;
+    private Marker2D _seatMarker; 
+    private Area2D _area;
 
 	public override void _Ready()
 	{
+		_seatMarker = GetNode<Marker2D>("SeatMarker");
+		player = GetNode<Player>("/root/Game/Player");
+        _area = GetNode<Area2D>("InteractArea");
+        _area.BodyEntered += OnBodyEntered;
+		_area.BodyExited += OnBodyExited;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		if (Boat_Can_Move == true)
+		if (Player_In_Boat == true)
 		{
 			float rotationDirection = Input.GetAxis("steer_left", "steer_right");
 			Rotation += rotationDirection * RotationSpeed * (float)delta;
@@ -22,6 +32,68 @@ public partial class Boat : CharacterBody2D
 			Velocity = Transform.Y * moveDirection * Speed;
 
 			MoveAndSlide();
+		}
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+    {
+        if (isPlayerNear == true && @event.IsActionPressed("interact"))
+        {
+            EnterBoat();
+        }
+
+		if (Player_In_Boat == true &&  @event.IsActionPressed("ui_accept"))
+		{
+            ExitBoat();
+		}
+    }
+
+	private void EnterBoat()
+    {
+        Player_In_Boat = true;
+        
+        player.SetPhysicsProcess(false); 
+        player.SetProcessInput(false);
+
+        player.GetNode<CollisionShape2D>("CollisionShape2D").Disabled = true; 
+
+        player.GetParent().RemoveChild(player);
+        _seatMarker.AddChild(player);
+
+        player.Position = Vector2.Zero;
+    }
+
+    private void ExitBoat()
+    {
+        Player_In_Boat = false;
+
+        _seatMarker.RemoveChild(player);
+
+
+        GetParent().AddChild(player);
+
+        player.GlobalPosition += new Vector2(50, 0);
+
+        player.SetPhysicsProcess(true);
+        player.SetProcessInput(true);
+
+        player.GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false; 
+        
+    }
+
+    private void OnBodyEntered(Node body)
+	{
+		if (body is Player)
+		{
+			isPlayerNear = true;
+		}
+	}
+
+    private void OnBodyExited(Node2D body)
+    {
+        if (body is Player)
+        {
+            isPlayerNear = false;
 		}
 	}
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -13,35 +13,33 @@ public partial class Player : CharacterBody2D
     public override void _PhysicsProcess(double delta)
     {
         var velocity = Vector2.Zero;
-        if (Player_Can_Move == true)
+
+        Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+        if (direction != Vector2.Zero)
         {
-            Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-            if (direction != Vector2.Zero)
-            {
-                velocity.X = direction.X * Speed * (float)delta;
-            }
-            else
-            {
-                velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-            }
+            velocity.X = direction.X * Speed * (float)delta;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+        }
 
-            if (direction != Vector2.Zero)
-            {
-                velocity.Y = direction.Y * Speed * (float)delta;
-            }
-            else
-            {
-                velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
-            }
-            var sprite2D = GetNode<Sprite2D>("Sprite2D");
+        if (direction != Vector2.Zero)
+        {
+            velocity.Y = direction.Y * Speed * (float)delta;
+        }
+        else
+        {
+            velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+        }
+        var sprite2D = GetNode<Sprite2D>("Sprite2D");
 
-            Position += velocity.Normalized() * Speed * (float)delta;
+        Position += velocity.Normalized() * Speed * (float)delta;
 
-            if (velocity.X != 0)
-            {
-                sprite2D.FlipH = false;
-                sprite2D.FlipH = velocity.X < 0;
-            }
+        if (velocity.X != 0)
+        {
+            sprite2D.FlipH = false;
+            sprite2D.FlipH = velocity.X < 0;
         }
 
         MoveAndSlide();
